01b1c7d07c9b57b1d6e29ed9137fe1b027e1f774
Dan Schmidt <dcschmid@gmail.com>
2020-08-08 08:46:54 -0400

    Add code to repro snappy bug

--- src/main/java/SnappyRepro/App.java
+++ src/main/java/SnappyRepro/App.java
@@ -1,5 +1,17 @@
 package SnappyRepro;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import org.xerial.snappy.Snappy;
+
 public class App {
-  public static void main(String[] args) {}
+  public static void main(String[] args) throws IOException {
+    ByteBuffer in = ByteBuffer.allocateDirect(100);
+    ByteBuffer out = ByteBuffer.allocateDirect(100);
+
+    in.put("Hello World!".getBytes(StandardCharsets.UTF_8));
+
+    Snappy.compress(in, out);
+  }
 }
